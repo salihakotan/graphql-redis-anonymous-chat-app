824df264b0b402d35221e6dde9b764dbd70c8044
Saliha Kotan <salihakotan77@gmail.com>
2024-06-19 04:09:33 +0300

    added gifs to header area

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='footer'>This project assignment has been prepared by <b>Saliha Kotan</b> for the 'Kodluyoruz' GraphQL course. An anonymous chat application that includes both backend and client and works in real time with GraphQL</div>
+    <div className='footer'>This project assignment has been prepared by <b>&nbsp;Saliha Kotan&nbsp;</b> for the 'Kodluyoruz' GraphQL course. An anonymous chat application that includes both backend and client and works in real time with GraphQL</div>
   )
 }
 
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import cuteGif from "../img/3.gif"
+import manGif from "../img/4.gif"
 
 function Header() {
   return (
     <div className='header'>
+            <img className='headerGifs cuteGif' src={cuteGif} alt='gif'/>
+
         <h1>Anonymous Chat App</h1>
+        <img className='headerGifs manGif' src={manGif} alt='gif'/>
     </div>
   )
 }
